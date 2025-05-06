2fdc5f82df4e0ca145d4f84f4a5fa6b10b617053
taha-aiplanet <taham655@gmail.com>
2025-05-06 13:55:12 +0800

    adding incognito

--- components/sidebar-user-nav.tsx
+++ components/sidebar-user-nav.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ChevronUp, Incognito } from 'lucide-react';
+import { ChevronUp } from 'lucide-react';
 import Image from 'next/image';
 import type { User } from 'next-auth';
 import { signOut } from 'next-auth/react';
